43467748ff819e9140a54e3ef2e42f88a733ff89
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-11-18 16:37:51 +0530

    Time: 0 ms (100%), Space: 14.6 MB (14.13%) - LeetHub

---
+++ 0936-rle-iterator/0936-rle-iterator.cpp
@@ -0,0 +1,21 @@
+class RLEIterator {
+    vector<int> encoding;
+    int index;
+public:
+    RLEIterator(vector<int>& encoding) {
+        this->encoding = encoding;
+        index = 0;
+    }
+    
+    int next(int n) {
+        while (index < encoding.size()) {
+            if (encoding[index] >= n) {
+                encoding[index] -= n;
+                return encoding[index + 1];
+            }
+            n -= encoding[index];
+            index += 2;
+        }
+        return -1;
+    }
+};
