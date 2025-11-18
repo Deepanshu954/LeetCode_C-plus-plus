eb002c0183fa2bd65a8e0f9a510bfd31f03e0d69
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-11-18 22:46:55 +0530

    Time: 0 ms (100%), Space: 10.3 MB (89.22%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canSplit(vector<int>& nums, int k, int maxSum) {
+        int subarrays = 1;
+        int currentSum = 0;
+        
+        for (int num : nums) {
+            if (currentSum + num > maxSum) {
+                subarrays++;
+                currentSum = num;
+                if (subarrays > k) return false;
+            } else {
+                currentSum += num;
+            }
+        }
+        
+        return true;
+    }
+    
+    int splitArray(vector<int>& nums, int k) {
+        int left = *max_element(nums.begin(), nums.end());
+        int right = accumulate(nums.begin(), nums.end(), 0);
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canSplit(nums, k, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+};
